ea5772d602d1eff82c6169ceb964ac23ad5eb9e3
Andrei Chelaru <andrei128@yahoo.com>
2015-05-28 08:01:01 +0300

    small updates

--- database_code/nucs_oracle_package.sql
+++ database_code/nucs_oracle_package.sql
@@ -1,17 +1,19 @@
-drop table		nucs_articles;
-drop table    nucs_cat_list;
-drop table		nucs_categories;
-drop table		nucs_users;
+drop table	nucs_articles;
+drop table	nucs_cat_list;
+drop table	nucs_categories;
+drop table	nucs_users;
+
+drop table	nucs_security_logs;
 
 drop sequence nucs_id_generator;
 create sequence nucs_id_generator	start with 1 increment by	1;
 
+drop trigger nucs_trig_suspicios_log;
 
 create table nucs_articles(
-		art_id		number primary key,
+		art_id	number primary key,
 		title	varchar2(512) not null,
 		link	varchar2(512) not null,
-		cat_list_id	number null,
 		autor	varchar2(128) null,
 		pubdate	date not null,
 		text	varchar2(2048)
@@ -36,6 +38,13 @@ create table nucs_users(
 	pass		varchar(32) not null
 );
 /
+
+create table nucs_security_logs(
+	art_id		number,
+	trig_date	date
+)
+/
+
 
 create or replace function nucs_get_index return number is
 begin
@@ -72,10 +81,10 @@ end;
 create or replace procedure add_rss_data(
 		v_title in	nucs_articles.title%type,
 		v_link	in	nucs_articles.link%type,
-		v_cat_list	in nucs_categ_list_type,
 		v_autor	in	nucs_articles.autor%type,
-		v_pubdate	in	nucs_articles.pubdate%type,
-		v_text	in	nucs_articles.text%type
+		v_pubdate	in	varchar2,
+		v_text	in	nucs_articles.text%type,
+    v_cat_list	in nucs_categ_list_type
 	) is
 
 	v_next_art_index number;
@@ -90,15 +99,33 @@ begin
 			v_next_art_index,
 			v_title,
 			v_link,
-			10,
 			v_autor,
-			v_pubdate,
+			sysdate,
 			v_text
 		);
 		
 	commit;
 	
 end;
+/
+
+
+create trigger nucs_trig_suspicios_log
+	after insert or update of title on nucs_articles
+      when(old.title like '%hack' )
+begin
+	insert into nucs_security_logs values( :new.art_id, sysdate );
+end;
+/
+
+create or replace view nucs_view_categories
+as
+	select cat_name from nucs_categories; 
+/
+
+create or replace view nucs_view_logs
+as
+	select art_id from nucs_security_logs;
 /
 
 commit
